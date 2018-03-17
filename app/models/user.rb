d6aea7eff70aba01728a73cf921cec0f609f69f8
Beau Winiger <bcwiniger@gmail.com>
2018-03-17 15:42:49 -0400

    finish out ch 14

--- app/models/user.rb
+++ app/models/user.rb
@@ -79,9 +79,12 @@ class User < ApplicationRecord
         reset_sent_at < 2.hours.ago
     end
 
-    # defines a proto-feed
+    # returns a user's status feed
     def feed
-        Micropost.where("user_id = ?", id)
+        following_ids = "SELECT followed_id FROM relationships
+                         WHERE  follower_id = :user_id"
+        Micropost.where("user_id IN (#{following_ids})
+                         OR user_id = :user_id", user_id: id)
     end
 
     # follows a user
